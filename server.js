b6749a1f370090faf66a6f10ae096304dd1d2962
Goutam96801 <gautamkumar96801@gmail.com>
2024-09-12 18:07:12 +0530

    ok

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const socketIO = require('socket.io');
 const app = express();
 const PORT = 5000;
 const allowedOrigins = [
-  'https://hash-zeeno-frontend.vercel.app/'
+  'https://hash-zeeno-frontend.vercel.app'
 ];
 
 app.use(cors({
